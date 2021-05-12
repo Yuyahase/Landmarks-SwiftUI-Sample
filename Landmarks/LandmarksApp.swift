dc86de5ae26eba6811b47df5b8a4891d9eafe517
Yuyahase <haseyuy@gmail.com>
2021-05-12 17:50:30 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yuya Hase on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
