898cb63be3e9c9845dba4068bff6195344e45969
Yuyahase <haseyuy@gmail.com>
2021-05-12 18:47:43 +0900

    Compose the Detail View

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -8,19 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    //Opaque Return Type
+    //ジェネリクスのようなもの
+    //some Viewは具体的な型は公開しないけど、Viewプロトコルに適合した何らかの型
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        //Implicit return from single expressions
+        //元々はreturn VStack
+        VStack {
+            MapView()
+                //Expands the view out of its safe area.
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
