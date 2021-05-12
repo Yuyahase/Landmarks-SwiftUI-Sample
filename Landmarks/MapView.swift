d80846ef1db6704e3b3000802f43f1b3e48a1be5
Yuyahase <haseyuy@gmail.com>
2021-05-12 18:27:20 +0900

    CircleImage

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Yuya Hase on 2021/05/12.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
